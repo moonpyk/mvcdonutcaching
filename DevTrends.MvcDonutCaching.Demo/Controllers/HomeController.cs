9b2653018a8a741b250aa83a195c1e3990562106
Clément Bourgeois <moonpyk@gmail.com>
2013-09-16 00:59:14 +0200

    Added more donut holes in the page, for the fun.

--- DevTrends.MvcDonutCaching.Demo/Controllers/HomeController.cs
+++ DevTrends.MvcDonutCaching.Demo/Controllers/HomeController.cs
@@ -1,19 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace DevTrends.MvcDonutCaching.Demo.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Simple");
+        }
+
         //
         // GET: /Home/
         [DonutOutputCache(Duration = 24 * 3600)]
-        public ActionResult Index()
+        public ActionResult Simple()
         {
             return View(DateTime.Now);
+        }
+
+        [ChildActionOnly, DonutOutputCache(Duration = 60)]
+        public ActionResult SimpleDonutOne()
+        {
+            return PartialView(DateTime.Now);
+        }
+
+        [ChildActionOnly]
+        public ActionResult SimpleDonutTwo()
+        {
+            return PartialView(DateTime.Now);
         }
     }
 }
