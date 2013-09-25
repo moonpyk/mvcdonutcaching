17e887102674f634304de8c81fab81b1ad1f2dde
Clément Bourgeois <clement.bourgeois@am-creations.fr>
2013-09-25 16:22:11 +0200

    Added docstrings for OutputCacheOptions.

--- DevTrends.MvcDonutCaching/OutputCacheOptions.cs
+++ DevTrends.MvcDonutCaching/OutputCacheOptions.cs
@@ -5,8 +5,14 @@ namespace DevTrends.MvcDonutCaching
     [Flags]
     public enum OutputCacheOptions
     {
-        None                  = 0x0,
+        None = 0x0,
+        /// <summary>
+        /// No matter what, never use the query string parameters to generate the cache key name
+        /// </summary>
         IgnoreQueryString = 0x1,
-        IgnoreFormData        = 0x2,
+        /// <summary>
+        /// No matter what, never use the Post data to generate the cache key name
+        /// </summary>
+        IgnoreFormData = 0x2,
     }
 }
