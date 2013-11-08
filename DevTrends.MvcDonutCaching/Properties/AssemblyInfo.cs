a3e645fc5c7335abbda9acb127e85461b4f1179d
Clément Bourgeois <moonpyk@gmail.com>
2013-11-08 22:25:10 +0100

    Version bump for prerelease.

--- DevTrends.MvcDonutCaching/Properties/AssemblyInfo.cs
+++ DevTrends.MvcDonutCaching/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ebcc3291-f04a-4511-b7eb-ddf57a74ada9")]
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
