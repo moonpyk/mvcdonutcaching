f7469b2d37b6e2179ad5ce614ef6431f1e90f1bd
mlidbo <magnus.lidbom@gmail.com>
2013-11-12 16:42:03 +0100

    Create an embryo for what could become the way to handle "It Just Works" for arbitrarily nested actions that just work if you apply the correct attribute.

---
+++ DevTrends.MvcDonutCaching/DonutOutputManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DevTrends.MvcDonutCaching
+{
+    public class Donut
+    {
+        internal readonly Donut Parent;
+        private TextWriter _realOutput;
+        public readonly ActionSettings ActionSettings;
+        public readonly ActionDescriptor ActionDescriptor;
+        public readonly IDictionary<string, object> ActionParameters; 
+        public readonly string CacheKey;
+        public readonly List<string> OutputList = new List<string>();
+        public readonly List<string> TrailingDonutList = new List<string>();
+
+        internal StringWriter Output { get; private set; }
+
+        public Donut(ActionExecutingContext context, Donut parent, TextWriter realOutput, ActionSettings actionSettings, string cacheKey)
+        {
+            ActionSettings = BuildActionSettings(context);
+            ActionDescriptor = context.ActionDescriptor;
+            ActionParameters = context.ActionParameters;
+            Parent = parent;
+            _realOutput = realOutput;
+            ActionSettings = actionSettings;
+            CacheKey = cacheKey;
+            Output = new StringWriter(CultureInfo.InvariantCulture);
+        }
+
+        internal void AddDonut(string child)
+        {
+            TrailingDonutList.Add(child);
+            FlushOutputSegment();
+        }
+
+        private void FlushOutputSegment()
+        {
+            var outputSegment = Output.ToString();
+            Output = new StringWriter(CultureInfo.InvariantCulture);
+            OutputList.Add(outputSegment);            
+            _realOutput.Write(outputSegment);
+        }
+
+        public void OnAfterExecute()
+        {
+            FlushOutputSegment();
+            Output = null;
+            _realOutput = null;
+        }
+
+
+        private static ActionSettings BuildActionSettings(ActionExecutingContext context)
+        {
+            return new ActionSettings()
+            {
+                ActionName = context.ActionDescriptor.ActionName,
+                ControllerName = context.ActionDescriptor.ControllerDescriptor.ControllerName,
+                RouteValues = new RouteValueDictionary(context.ActionParameters)
+            };
+        }
+    }
+
+    /// <summary>
+    /// Manages the respons output and the stack of Donuts as the request recurses down the action methods.
+    /// </summary>
+    public class DonutOutputManager
+    {
+        /// <summary>
+        /// The currently executing Action filter with an effective attribute(On the action, controller, or parent controller)
+        /// Actions with no effective attribute will be included in the output of their parent action.
+        /// </summary>
+        private Donut _current;
+
+        private int _depth;
+
+        private static readonly Guid OutPutManagerId = Guid.Parse("ADECE17F-38DD-420D-B7B8-AA15430A9920");
+        internal TextWriter OriginalOutput;
+
+        public static void Push(ActionExecutingContext context, string cacheKey)
+        {
+            var manager = context.HttpContext.Items[OutPutManagerId] as DonutOutputManager;
+            if(manager == null)
+            {
+                context.HttpContext.Items[OutPutManagerId] = manager = new DonutOutputManager();
+                manager.OriginalOutput = context.HttpContext.Response.Output;
+                manager._current = new Donut(context, null, manager.OriginalOutput, BuildActionSettings(context), cacheKey);                
+            }
+            else
+            {
+                var pushed = new Donut(context, manager._current, manager.OriginalOutput, BuildActionSettings(context), cacheKey);
+                manager._current.AddDonut(pushed.CacheKey);
+                manager._current = pushed;
+            }
+            manager._depth++;
+            context.HttpContext.Response.Output = manager._current.Output;
+        }
+
+        private static ActionSettings BuildActionSettings(ActionExecutingContext context)
+        {
+            return new ActionSettings()
+                   {
+                       ActionName = context.ActionDescriptor.ActionName,
+                       ControllerName = context.ActionDescriptor.ControllerDescriptor.ControllerName,
+                       RouteValues = new RouteValueDictionary(context.ActionParameters)
+                   };
+        }
+
+        public static Donut Pop(ControllerContext context)
+        {
+            var manager = (DonutOutputManager)context.HttpContext.Items[OutPutManagerId];
+            manager._depth--;
+            var popped = manager._current;
+            popped.OnAfterExecute();
+            if (manager._depth == 0)
+            {
+                manager._current = null;
+                context.HttpContext.Items[OutPutManagerId] = null;
+                context.HttpContext.Response.Output = manager.OriginalOutput;
+            }
+            else
+            {
+                manager._current = popped.Parent;
+                context.HttpContext.Response.Output = manager._current.Output;
+            }            
+            return popped;
+        }
+    }
+}
---
+++ MvcDonutCaching.Tests/DonutOutputManagerTests.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevTrends.MvcDonutCaching;
+using Moq;
+using NUnit.Framework;
+
+namespace MvcDonutCaching.Tests
+{
+    [TestFixture]
+    public class DonutOutputManagerTests
+    {
+        [Test]
+        public void ReplacesAndRestoresResponseOutPutCorrectly()
+        {
+            var context = CreateMockActionExecutingControllerContext().Object;
+
+            var rootOutput = new StringWriter();
+            context.HttpContext.Response.Output = rootOutput;
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName:"dummy", actionName:"level1");
+            context.ActionParameters = new Dictionary<string, object>(){ {"title", "level1"} };
+            DonutOutputManager.Push(context, cacheKey: "level1");
+            Assert.That(context.HttpContext.Response.Output, Is.Not.SameAs(rootOutput), "output should have been replaced");
+            context.HttpContext.Response.Output.WriteLine("");
+            context.HttpContext.Response.Output.WriteLine("1");
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level2");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level2" } };
+            DonutOutputManager.Push(context, cacheKey: "level2");
+            context.HttpContext.Response.Output.WriteLine(" 2");
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level3");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level3" } };
+            DonutOutputManager.Push(context, cacheKey: "level3");
+            context.HttpContext.Response.Output.WriteLine("  3");
+            DonutOutputManager.Pop(context);
+            context.HttpContext.Response.Output.WriteLine(" 2");
+            DonutOutputManager.Pop(context);
+            context.HttpContext.Response.Output.WriteLine("1");
+            DonutOutputManager.Pop(context);
+            Assert.That(context.HttpContext.Response.Output, Is.SameAs(rootOutput), "output should have been restored");
+            Assert.That(rootOutput.ToString(),
+                Is.EqualTo(
+                    @"
+1
+ 2
+  3
+ 2
+1
+"));
+        }
+
+        [Test]
+        public void SetsDonutOutputListAndDonutListCorrectly()
+        {
+            var context = CreateMockActionExecutingControllerContext().Object;
+
+            var rootOutput = new StringWriter();
+            context.HttpContext.Response.Output = rootOutput;
+
+            Donut level1Donut, level2Donut1, level2Donut2, level3Donut1, level3Donut2 = null;
+            const string level1StartOutput = "_1",
+                level1EndOutput = "1_",
+                level2StartOutput = "_2",
+                level2EndOutput = "2_",
+                level3Output = "_3_";
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level1");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level1" } };
+            DonutOutputManager.Push(context, cacheKey: "level1"); //Level1
+            Assert.That(context.HttpContext.Response.Output, Is.Not.SameAs(rootOutput), "output should have been replaced");
+            context.HttpContext.Response.Output.Write(level1StartOutput);
+
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level2");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level2_1" } };
+            DonutOutputManager.Push(context, cacheKey: "level2_1"); //Level2
+            context.HttpContext.Response.Output.Write(level2StartOutput);
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level3");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level3" } };
+            DonutOutputManager.Push(context, cacheKey: "level3_1"); //Level3
+            context.HttpContext.Response.Output.Write(level3Output);
+            level3Donut1 = DonutOutputManager.Pop(context); //Level2
+            context.HttpContext.Response.Output.Write(level2EndOutput);
+            level2Donut1 = DonutOutputManager.Pop(context); //Level1
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level2");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level2_1" } };
+            DonutOutputManager.Push(context, cacheKey: "level2_2"); //Level2
+            context.HttpContext.Response.Output.Write(level2StartOutput);
+
+
+            context.ActionDescriptor = new StaticActionDescriptor(controllerName: "dummy", actionName: "level3");
+            context.ActionParameters = new Dictionary<string, object>() { { "title", "level3_2" } };
+            DonutOutputManager.Push(context, cacheKey: "level3_2"); //Level3
+            context.HttpContext.Response.Output.Write(level3Output);
+            level3Donut2 = DonutOutputManager.Pop(context); //Level2
+            context.HttpContext.Response.Output.Write(level2EndOutput);
+            level2Donut2 = DonutOutputManager.Pop(context); //Level1
+
+            context.HttpContext.Response.Output.Write(level1EndOutput);
+            level1Donut = DonutOutputManager.Pop(context); //Done
+
+            Assert.That(level3Donut1.OutputList, Is.EqualTo(new[] {level3Output}));
+            Assert.That(level3Donut1.TrailingDonutList.Count, Is.EqualTo(0));
+
+            Assert.That(level3Donut2.OutputList, Is.EqualTo(new[] {level3Output}));
+            Assert.That(level3Donut2.TrailingDonutList.Count, Is.EqualTo(0));
+
+            Assert.That(level2Donut1.OutputList, Is.EqualTo(new[] {level2StartOutput, level2EndOutput}));
+            Assert.That(level2Donut1.TrailingDonutList.Count, Is.EqualTo(1));
+            Assert.That(level2Donut1.TrailingDonutList[0], Is.EqualTo(level3Donut1.CacheKey));
+
+            Assert.That(level2Donut2.OutputList, Is.EqualTo(new[] {level2StartOutput, level2EndOutput}));
+            Assert.That(level2Donut2.TrailingDonutList.Count, Is.EqualTo(1));
+            Assert.That(level2Donut2.TrailingDonutList[0], Is.EqualTo(level3Donut2.CacheKey));
+
+            Assert.That(level1Donut.OutputList, Is.EqualTo(new[] {level1StartOutput, "", level1EndOutput}));
+            Assert.That(level1Donut.TrailingDonutList.Count, Is.EqualTo(2));
+            Assert.That(level1Donut.TrailingDonutList[0], Is.EqualTo(level2Donut1.CacheKey));
+            Assert.That(level1Donut.TrailingDonutList[1], Is.EqualTo(level2Donut2.CacheKey));
+        }
+
+        [Test]
+        public void henEveryThingIsCached()
+        {
+            var context = CreateMockActionExecutingControllerContext().Object;
+
+            var rootOutput = new StringWriter();
+            context.HttpContext.Response.Output = rootOutput;
+
+            Donut level1Donut, level2Donut1, level2Donut2, level3Donut1, level3Donut2 = null;
+            const string level1StartOutput = "_1",
+                level1EndOutput = "1_",
+                level2StartOutput = "_2",
+                level2EndOutput = "2_",
+                level3Output = "_3_";
+
+
+        }
+
+        private static Mock<ActionExecutingContext> CreateMockActionExecutingControllerContext()
+        {
+            var response = new Mock<HttpResponseBase>(MockBehavior.Strict);
+            response.SetupProperty(resp => resp.Output);
+
+            var httpContextMock = new Mock<HttpContextBase>(MockBehavior.Strict);
+            httpContextMock.Setup(http => http.Response).Returns(response.Object);
+            IDictionary items = new Hashtable();
+            httpContextMock.Setup(http => http.Items).Returns(items);
+
+            var contextMock = new Mock<ActionExecutingContext>(MockBehavior.Strict);
+            contextMock.Setup(context => context.HttpContext).Returns(httpContextMock.Object);
+            contextMock.SetupProperty(context => context.ActionDescriptor);
+            contextMock.SetupProperty(context => context.ActionParameters);
+
+            return contextMock;
+        }
+    }
+
+    public class StaticActionDescriptor : ActionDescriptor
+    {
+        private readonly string _controllerName;
+        private readonly string _actionName;
+        private ControllerDescriptor _controllerDescriptor;
+
+        public StaticActionDescriptor(string controllerName, string actionName)
+        {
+            _controllerName = controllerName;
+            _actionName = actionName;
+            _controllerDescriptor = new StaticControllerDescriptor(_controllerName);
+        }
+
+        override public object Execute(ControllerContext controllerContext, IDictionary<string, object> parameters)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        override public ParameterDescriptor[] GetParameters()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        override public string ActionName { get { return  _actionName; } }
+
+        override public ControllerDescriptor ControllerDescriptor { get { return _controllerDescriptor; } }
+    }
+
+    public class StaticControllerDescriptor : ControllerDescriptor
+    {
+        private readonly string _controllerName;
+
+        public StaticControllerDescriptor(string controllerName)
+        {
+            _controllerName = controllerName;
+        }
+
+        override public ActionDescriptor FindAction(ControllerContext controllerContext, string actionName)
+        {
+            throw new NotImplementedException();
+        }
+
+        override public string ControllerName { get { return _controllerName; } }
+
+        override public ActionDescriptor[] GetCanonicalActions()
+        {
+            throw new NotImplementedException();
+        }
+
+        override public Type ControllerType { get { throw new NotImplementedException(); } }
+    }
+}
