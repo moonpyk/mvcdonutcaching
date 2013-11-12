b6dc0e0779d742909611afa6b9dc4ab75ca64505
Clément Bourgeois <moonpyk@gmail.com>
2013-11-12 22:13:01 +0100

    Working on the documentation.

--- DevTrends.MvcDonutCaching/Interfaces/IDonutHoleFiller.cs
+++ DevTrends.MvcDonutCaching/Interfaces/IDonutHoleFiller.cs
@@ -5,12 +5,12 @@ namespace DevTrends.MvcDonutCaching
     public interface IDonutHoleFiller
     {
         /// <summary>
-        /// Removes the donut hole wrappers.
+        /// Implentations should remove the donut hole wrappers.
         /// </summary>
         /// <param name="content">The content.</param>
         /// <param name="filterContext">The filter context.</param>
         /// <param name="options">The output cache options.</param>
-        /// <returns></returns>
+        /// <returns>A donut hole wrapper free string</returns>
         string RemoveDonutHoleWrappers(string content, ControllerContext filterContext, OutputCacheOptions options);
         
         /// <summary>
@@ -19,7 +19,7 @@ namespace DevTrends.MvcDonutCaching
         /// <param name="content">The content.</param>
         /// <param name="filterContext">The filter context.</param>
         /// <param name="options">The output cache options.</param>
-        /// <returns></returns>
+        /// <returns>A string containing the donut holes replaced by content.</returns>
         string ReplaceDonutHoleContent(string content, ControllerContext filterContext, OutputCacheOptions options);
     }
 }
