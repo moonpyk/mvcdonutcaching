48a8ce7701ce615a5af0dac19fe4612e5772803b
mlidbo <magnus.lidbom@gmail.com>
2013-11-15 04:38:38 +0100

    Cleanup

--- DevTrends.MvcDonutCaching/Mlidbom/AutoOutputCacheAttribute.cs
+++ DevTrends.MvcDonutCaching/Mlidbom/AutoOutputCacheAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Web.Mvc;
 
 namespace DevTrends.MvcDonutCaching.Mlidbom
@@ -7,28 +6,12 @@ namespace DevTrends.MvcDonutCaching.Mlidbom
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
     public class AutoOutputCacheAttribute : ActionFilterAttribute
     {
-        private TextWriter _oldOutput;
-        private string _action;
-        private StringWriter _myOutPut;
-
         public AutoOutputCacheAttribute()
         {
-            Order = int.MaxValue;
-            bool breakpoint = true;
         }
         override public void OnActionExecuting(ActionExecutingContext filterContext)
         {
             DonutOutputManager.ActionExecuting(filterContext);
-        }
-
-        override public void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            base.OnActionExecuted(filterContext);
-        }
-
-        override public void OnResultExecuting(ResultExecutingContext filterContext)
-        {
-            base.OnResultExecuting(filterContext);
         }
 
         override public void OnResultExecuted(ResultExecutedContext filterContext)
--- DevTrends.MvcDonutCaching/Mlidbom/DonutOutputManager.cs
+++ DevTrends.MvcDonutCaching/Mlidbom/DonutOutputManager.cs
@@ -1,63 +1,36 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace DevTrends.MvcDonutCaching.Mlidbom
 {
-    /// <summary>
-    /// Manages the respons output and the stack of Donuts as the request recurses down the action methods.
-    /// </summary>
     public class DonutOutputManager
     {
-        /// <summary>
-        /// The currently executing Action filter with an effective attribute(On the action, controller, or parent controller)
-        /// Actions with no effective attribute will be included in the output of their parent action.
-        /// </summary>
-        private Donut _current;
+        private static readonly Guid DonutStackId = Guid.Parse("BBD4587E-4905-43CB-9BEC-6B68D3704F4E");
 
-        private int _depth;
-
-        private static readonly Guid OutPutManagerId = Guid.Parse("ADECE17F-38DD-420D-B7B8-AA15430A9920");
-        internal TextWriter OriginalOutput;
+        private static Stack<Donut> DonutStack(ControllerContext context)
+        {
+            Contract.Parameter.NotNull(context); 
+            var donutStack = context.HttpContext.Items[DonutStackId] as Stack<Donut>;
+            if (donutStack == null)
+            {
+                context.HttpContext.Items[DonutStackId] = donutStack = new Stack<Donut>();
+            }
+            return donutStack;
+        }
 
         public static void ActionExecuting(ActionExecutingContext context)
         {
-            Contract.Parameter.NotNull(context);
-            var manager = context.HttpContext.Items[OutPutManagerId] as DonutOutputManager;
-            if(manager == null)
-            {
-                context.HttpContext.Items[OutPutManagerId] = manager = new DonutOutputManager();
-                manager._current = new Donut(context, null);                
-            }
-            else
-            {
-                var pushed = new Donut(context, manager._current);
-                manager._current = pushed;
-            }
-            manager._depth++;
+            var donutStack = DonutStack(context);
+            var pushed = new Donut(context, donutStack.Count > 0 ? donutStack.Peek() : null);
+            donutStack.Push(pushed);
         }
 
         public static Donut ResultExecuted(ControllerContext context)
-        {
-            Contract.Parameter.NotNull(context);
-            var manager = (DonutOutputManager)context.HttpContext.Items[OutPutManagerId];
-            if(manager == null)
-            {
-                throw new InvalidOperationException("Attempt to pop when no donut has been pushed during this request.");
-            }
-            manager._depth--;
-            var popped = manager._current;
-            popped.ResultExecuted();
-            if (manager._depth == 0)
-            {
-                manager._current = null;
-                context.HttpContext.Items[OutPutManagerId] = null;
-            }
-            else
-            {
-                manager._current = popped.Parent;
-            }            
-            return popped;
+        {            
+            var executed = DonutStack(context).Pop();;
+            executed.ResultExecuted();
+            return executed;
         }
     }
 }
