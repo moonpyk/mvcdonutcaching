9727c7ce32d3a8fb75199fe11714a740a1ce39ee
mlidbo <magnus.lidbom@gmail.com>
2013-11-15 21:14:40 +0100

    Make exploratory view output more html compatible and readable in browser.

--- MvcDonutCaching.Tests.Web/Controllers/WriteOrderAnalysisHelperController.cs
+++ MvcDonutCaching.Tests.Web/Controllers/WriteOrderAnalysisHelperController.cs
@@ -6,16 +6,19 @@ namespace MvcDonutCaching.Tests.Web.Controllers
     [AutoOutputCache(Duration = 3600)]
     public class WriteOrderAnalysisHelperController : Controller
     {
+        [AutoOutputCache(Duration = 2)]
         public ActionResult Index()
         {
             return View();
         }
 
+        [AutoOutputCache(Duration = 4)]
         public ActionResult Level1()
         {
             return View();
         }
 
+        [AutoOutputCache(Duration = 6)]
         public ActionResult Level2()
         {
             return View();
