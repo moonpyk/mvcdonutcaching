8fe0e872564e20e09e920d17404b8eb7a8b09916
mlidbo <magnus.lidbom@gmail.com>
2013-11-16 01:06:18 +0100

    Write test that exposes bug which occurs when using page with nested layouts and sections.

---
+++ MvcDonutCaching.Tests/PageWithNestedLayoutsAndSectionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Caching;
+using NUnit.Framework;
+
+namespace MvcDonutCaching.Tests
+{
+    //Render order is all sorts of funny in this scenario :)
+    [TestFixture]
+    public class PageWithNestedLayoutsAndSectionsTests : TestsBase
+    {
+        private const string ActionPath = "/PageWithNestedLayoutsAndSections";
+
+        [Test]
+        public void RendersIdenticalContentFromCacheAsOnInitialRender()
+        {
+            var initialContent = GetUrlContent(ActionPath);
+            var cachedContent = GetUrlContent(ActionPath);
+
+            Assert.That(initialContent, Is.EqualTo(cachedContent));
+        }
+    }
+}
