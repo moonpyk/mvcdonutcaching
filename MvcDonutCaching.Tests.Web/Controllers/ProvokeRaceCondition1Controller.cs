49156a243315cc26eb5a901d08110418a03eeae1
mlidbo <magnus.lidbom@gmail.com>
2013-11-17 14:49:23 +0100

    Write test that provokes a serverside race condition causing an exception.

---
+++ MvcDonutCaching.Tests.Web/Controllers/ProvokeRaceCondition1Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using DevTrends.MvcDonutCaching.Mlidbom;
+
+namespace MvcDonutCaching.Tests.Web.Controllers
+{
+    [AutoOutputCache(Duration = 0)]
+    public class ProvokeRaceCondition1Controller : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [AutoOutputCache(Duration = 3600)]
+        public ActionResult TopLayoutHeader()
+        {
+            return View();
+        }
+
+        [AutoOutputCache(Duration = 3600)]
+        public ActionResult TopLayoutFooter()
+        {
+            return View();
+        }
+
+        [AutoOutputCache(Duration = 3600)]
+        public ActionResult NestedLayoutHeader()
+        {
+            return View();
+        }
+
+        [AutoOutputCache(Duration = 3600)]
+        public ActionResult NestedLayoutFooter()
+        {
+            return View();
+        }
+
+        [AutoOutputCache(Duration = 3600)]
+        public ActionResult PageBody()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcDonutCaching.Tests/Mlidbom/ProvokeRaceCondition1Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Threading;
+using System.Threading.Tasks;
+using NCrunch.Framework;
+using NUnit.Framework;
+
+namespace MvcDonutCaching.Tests.Mlidbom
+{
+    [TestFixture, ExclusivelyUses(Controller)]
+    public class ProvokeRaceCondition1Tests : ControllerTestBase
+    {
+        private const string Controller = "ProvokeRaceCondition1";
+        override protected string ControllerName { get { return Controller; } }
+
+        [Test]
+        public void KeepsRenderingIdenticalContentWhenMultipleThreadsHammerRequests()
+        {
+            Console.WriteLine("Starting");
+
+            long requestsMade = 0;
+            try
+            {
+                ThreadingHelper.HammerMultiThreadedFor(
+                    TimeSpan.FromMilliseconds(1000),
+                    out requestsMade, () => ExecuteDefaultAction());
+            }
+            catch(AggregateException exceptions)
+            {
+                exceptions.Handle(e =>
+                                  {
+                                      if(e.InnerException is WebException)
+                                      {
+                                          Console.WriteLine("Server side error message:");
+                                          Console.WriteLine(e.Message);
+                                      }
+                                      return false;
+                                  });
+            }
+
+            Console.WriteLine("Made {0} requests without errors :)", requestsMade);
+        }
+    }
+}
