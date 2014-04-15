579ea19c568f87a9e858e703cf1d8c6df6a9d780
Clément Bourgeois <moonpyk@gmail.com>
2014-04-15 21:20:33 +0200

    Added a test area in the demo project to help fixing #14.

---
+++ DevTrends.MvcDonutCaching.Demo/Areas/SubArea/Controllers/SubHomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevTrends.MvcDonutCaching.Demo.Controllers;
+
+namespace DevTrends.MvcDonutCaching.Demo.Areas.SubArea.Controllers
+{
+    public class SubHomeController : ApplicationController
+    {
+        //
+        // GET: /SubArea/Home/
+        [DonutOutputCache(Duration = 24 * 3600)]
+        public ActionResult Index()
+        {
+            return View(DateTime.Now);
+        }
+
+    }
+}
---
+++ DevTrends.MvcDonutCaching.Demo/Areas/SubArea/SubAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DevTrends.MvcDonutCaching.Demo.Areas.SubArea
+{
+    public class SubAreaAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SubArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SubArea_default",
+                "SubArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
