1e85634429cd76f9eab23d76831d16098a490cc4
Clément Bourgeois <moonpyk@gmail.com>
2016-04-02 11:48:44 +0200

    Version bump

--- DevTrends.MvcDonutCaching/Properties/AssemblyInfo.cs
+++ DevTrends.MvcDonutCaching/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DevTrends")]
 [assembly: AssemblyProduct("DevTrends.MvcDonutCaching")]
-[assembly: AssemblyCopyright("Copyright 2014 © MvcDonutCaching contributors")]
+[assembly: AssemblyCopyright("Copyright 2016 © MvcDonutCaching contributors")]
 // [assembly: AssemblyTrademark("")]
 // [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ebcc3291-f04a-4511-b7eb-ddf57a74ada9")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
 
 #if RELEASE_PUBLIC
     [assembly: AssemblyKeyFile(@"C:\Users\moonpyk\Documents\Clés\Code\Code.snk")]
